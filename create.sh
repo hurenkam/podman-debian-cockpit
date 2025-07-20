777bde658fe96121e8291fca050c08999a68c9dc
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2025-07-20 22:54:18 +0200

    fix authorized_keys being empty after creation

--- create.sh
+++ create.sh
@@ -2,7 +2,7 @@
 podman build . -t debian-cockpit
 
 # Create the container:
-podman create --name=debian-cockpit --hostname=debian-cockpit --publish=2222:22 --publish=9090:9090 debian-cockpit
+podman create --replace --name=debian-cockpit --hostname=debian-cockpit --publish=2222:22 --publish=9090:9090 debian-cockpit
 
 # Start the container:
 podman container start debian-cockpit
@@ -10,5 +10,5 @@ podman container start debian-cockpit
 # Allow root login
 podman container exec -ti debian-cockpit passwd
 podman container exec debian-cockpit rm -f /etc/cockpit/disallowed-users
-cat ~/.ssh/*.pub | podman container exec debian-cockpit bash -c 'cat - >> /root/.ssh/authorized_keys'
+cat ~/.ssh/*.pub | podman container exec -i debian-cockpit bash -c 'cat - >> /root/.ssh/authorized_keys'
 
