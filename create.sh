f5b05fc829b7507d6db34e61195e4622a1558297
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2025-07-20 20:29:22 +0200

    update create script

--- create.sh
+++ create.sh
@@ -4,11 +4,11 @@ podman build . -t debian-cockpit
 # Create the container:
 podman create --name=debian-cockpit --hostname=debian-cockpit --publish=2222:22 --publish=9090:9090 debian-cockpit
 
+# Start the container:
+podman container start debian-cockpit
+
 # Allow root login
 podman container exec -ti debian-cockpit passwd
-podman conatiner exec -ti debian-cockpit rm -f /etc/cockpit/disallowed-users
-cat ~/.ssh/*.pub | podman container exec -t debian-cockpit bash -c 'cat - >> /root/.ssh/authorized_keys'
-
-### Run the image:
-`podman run --name debian-cockpit --publish=2222:22 --publish=9090:9090 docker.io/hurenkam/debian-cockpit:latest`
+podman container exec debian-cockpit rm -f /etc/cockpit/disallowed-users
+cat ~/.ssh/*.pub | podman container exec debian-cockpit bash -c 'cat - >> /root/.ssh/authorized_keys'
 
